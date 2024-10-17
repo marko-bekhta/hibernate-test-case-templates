184698e74cdd400b7ae08e47e3858b03b417f306
marko-bekhta <marko.prykladna@gmail.com>
2024-10-17 09:55:44 +0200

    Discourse example 10454
    https://discourse.hibernate.org/t/how-to-use-hibernate-search-elastic-backend/10454

--- search/hibernate-search-7/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/YourIT.java
+++ search/hibernate-search-7/orm-elasticsearch/src/test/java/org/hibernate/search/bugs/YourIT.java
@@ -6,41 +6,103 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.search.engine.search.predicate.SearchPredicate;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 
 public class YourIT extends SearchTestBase {
 
 	@Override
 	public Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] { YourAnnotatedEntity.class };
+		return new Class<?>[] { Case.class, Patent.class };
 	}
 
 	@Test
 	public void testYourBug() {
 		try ( Session s = getSessionFactory().openSession() ) {
-			YourAnnotatedEntity yourEntity1 = new YourAnnotatedEntity( 1L, "Jane Smith" );
-			YourAnnotatedEntity yourEntity2 = new YourAnnotatedEntity( 2L, "John Doe" );
+			Case case1 = new Case( 1L, "case 1" );
+			Case case2 = new Case( 2L, "case 2" );
+			Patent patent1 = new Patent( 3L, "case 10", "patent1" );
 
 			Transaction tx = s.beginTransaction();
-			s.persist( yourEntity1 );
-			s.persist( yourEntity2 );
+			s.persist( case1 );
+			s.persist( case2 );
+			s.persist( patent1 );
 			tx.commit();
 		}
 
 		try ( Session session = getSessionFactory().openSession() ) {
 			SearchSession searchSession = Search.session( session );
 
-			List<YourAnnotatedEntity> hits = searchSession.search( YourAnnotatedEntity.class )
-					.where( f -> f.match().field( "name" ).matching( "smith" ) )
+			List<Long> hits = searchSession.search( Case.class )
+					.select( f -> f.id( Long.class ) )
+					.where( f -> f.match().field( "name" ).matching( "case" ) )
 					.fetchHits( 20 );
 
 			assertThat( hits )
-					.hasSize( 1 )
-					.element( 0 ).extracting( YourAnnotatedEntity::getId )
-					.isEqualTo( 1L );
+					.hasSize( 3 )
+					.containsOnly( 1L, 2L, 3L );
+
+			SearchPredicate predicate = searchSession.scope( Case.class )
+					.predicate().match().field( "name" ).matching( "case" ).toPredicate();
+
+			hits = searchSession.search( Case.class )
+					.select( f -> f.id( Long.class ) )
+					.where( predicate )
+					.fetchHits( 20 );
+
+			assertThat( hits )
+					.hasSize( 3 )
+					.containsOnly( 1L, 2L, 3L );
+		}
+	}
+
+	@Entity
+	@Indexed
+	@Inheritance(strategy = InheritanceType.JOINED)
+	@Table(name = "t_case")
+	public static class Case {
+
+		@Id
+		@DocumentId
+		public Long id;
+
+		@FullTextField
+		public String name;
+
+		public Case() {
+		}
+
+		public Case(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+	}
+
+	@Entity
+	@Indexed
+	public static class Patent extends Case {
+
+		@FullTextField
+		String string;
+
+		public Patent() {
+		}
+
+		public Patent(Long id, String name, String string) {
+			super( id, name );
+			this.string = string;
 		}
 	}
 
